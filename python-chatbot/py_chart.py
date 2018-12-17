6e6e6139f3c7f4d8135c5a59b2e65d3cf0de8310
Gyuseok Kim <34904741+kim6394@users.noreply.github.com>
2018-12-17 16:17:12 +0900

    Create py_chart.py

---
+++ python-chatbot/py_chart.py
@@ -0,0 +1,42 @@
+from elice_utils import EliceUtils
+
+elice_utils = EliceUtils()
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    print("----산포도1----")
+
+    x = np.arange(0, 100)
+    y = np.arange(0, 100)
+    print(x)
+    plt.plot(x, y, "o")     #plt.plot(x, y, "k") 
+
+    print("----산포도2----")
+
+    line = plt.figure()
+
+    np.random.seed(5)
+    x = np.arange(1, 101)
+    y = 20 + 3 * x + np.random.normal(0, 40, 100)
+    plt.plot(x, y, "o")
+    plt.savefig("image.svg", format="svg")
+    elice_utils.send_image("image.svg")
+
+
+    print("----파이차트----")
+
+    slices_hours = [5, 3, 8]
+    activities = ["Sleep", "Work", "Nothing"]
+
+    color = ["gray", "blue", "red"]
+
+    plt.pie(slices_hours, labels=activities, colors=color, startangle=90, autopct='%.1f%%')
+
+    plt.savefig("image.svg", format="svg")
+    elice_utils.send_image("image.svg")
+
+
+if __name__ == "__main__":
+    main()
