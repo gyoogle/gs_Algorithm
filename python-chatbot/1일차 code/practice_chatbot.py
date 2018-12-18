5a1b2946b0ffd0b1a6deb6153a26e772b13b2bd4
Gyuseok Kim <34904741+kim6394@users.noreply.github.com>
2018-12-18 10:32:29 +0900

    Update practice_chatbot.py

--- python-chatbot/1일차 code/practice_chatbot.py
+++ python-chatbot/1일차 code/practice_chatbot.py
@@ -1,6 +1,7 @@
 from elice_utils import EliceUtils
 import urllib.request
 from bs4 import BeautifulSoup
+from urllib import parse
 
 elice_utils = EliceUtils()
 
@@ -12,21 +13,25 @@ def main():
     
     encode = parse.quote(search)
     
-    for i in range(0,10):
+    listNews = []
+    image = []
+    
+    for i in range(0,3):
         url = "https://search.naver.com/search.naver?&where=news&query=" + encode + "&sm=tab_pge&sort=1&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid=&nso=so:dd,p:all,a:all&mynews=0&start=" + str((i*10)+1) +"&refresh_start=0"
         soup = urllib.request.urlopen(url).read()
         #print(soup)
         # URL 주소에 있는 HTML 코드를 soup에 저장합니다.
         temp = BeautifulSoup(soup, "html.parser")
-        spans = temp.find_all("a", class_=" _sp_each_title")
-    
-    
-        list = []
+        news = temp.find_all("a", class_=" _sp_each_title")
+        images = temp.find_all("image")
+        
+        for naver_text in news:
+            listNews.append(naver_text.get_text())
+            
+        for image in news:
+            listNews.append(image)
 
-        for naver_text in spans:
-            list.append(naver_text.get_text())
-
-        print(list)
+    print(listNews)
 
 
 if __name__ == "__main__":
