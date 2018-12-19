e3e54809d2e298e0145455b0753a009626222f59
Gyuseok Kim <34904741+kim6394@users.noreply.github.com>
2018-12-19 15:02:21 +0900

    Update selenium.py

--- python-chatbot/selenium.py
+++ python-chatbot/selenium.py
@@ -1,4 +1,5 @@
-# selenium을 활용해 로그인 진행하기
+# selenium을 활용해 로그인 진행하기 + 게시물 작성
+
 
 from selenium import webdriver
 
@@ -13,3 +14,15 @@ driver.find_element_by_name('memberID').send_keys(id)
 driver.find_element_by_name('memberPassword').send_keys(pw)
 
 driver.find_element_by_xpath('//*[@id="blackBox"]/input[1]').click()
+driver.find_element_by_xpath('//*[@id="navigationBox"]/a[4]').click()
+driver.find_element_by_xpath('//*[@id="blackBox"]/input[1]').click()
+
+title = "test"
+content = "test test"
+source = "hello world"
+
+driver.find_element_by_name('talkTitle').send_keys(title)
+driver.find_element_by_name('talkContent').send_keys(content)
+driver.find_element_by_name('talkSourceCode').send_keys(source)
+
+driver.find_element_by_xpath('//*[@id="blackBox"]/input').click()
