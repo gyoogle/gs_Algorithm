9b8b1d69adb126896e9cee5efcb4cdf1578d222c
Gyuseok Kim <34904741+kim6394@users.noreply.github.com>
2019-04-13 12:00:39 +0900

    벽부수고이동하기_규석(백준2206)

--- Algorithm(Study)/문제 풀이/백준/Main_2206_벽부수고이동하기_김규석.java
+++ Algorithm(Study)/문제 풀이/백준/Main_2206_벽부수고이동하기_김규석.java
@@ -2,102 +2,96 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main_2206_벽부수고이동하기_김규석 {
-
-	static class H {
+	
+	static class Place {
 		int y;
 		int x;
-		int dist;
-		int drill;
+		int dis; // 이동거리
+		int drill; // 공사횟수
 		
-		public H(int y, int x, int dist, int drill) {
-			super();
+		public Place(int y, int x, int dis, int drill) {
 			this.y = y;
 			this.x = x;
-			this.dist = dist;
+			this.dis = dis;
 			this.drill = drill;
 		}
 	}
 
-	static int N, M;
-	static int[][] map;
-	static int[][] copy;
+	static int N,M,ans;
+	static int[][] map, visit;
 	
-
-	static int[] dy = { -1, 1, 0, 0 };
-	static int[] dx = { 0, 0, -1, 1 };
-
-	static int result;
-
+	static int[] dy = {-1,1,0,0}; // 상,하,좌,우
+	static int[] dx = {0,0,-1,1};
+	
 	public static void main(String[] args) throws Exception {
-
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		map = new int[N][M];
-		copy = new int[N][M];
-
+		
+		String[] str = br.readLine().split(" ");
+		
+		N = Integer.parseInt(str[0]); // 행
+		M = Integer.parseInt(str[1]); // 열
+		
+		map = new int[N][M]; // 초기화
+		visit = new int[N][M]; // 초기화
+		
 		for (int i = 0; i < N; i++) {
-			String[] str = br.readLine().split("");
+			str = br.readLine().split("");
 			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(str[j]);
-				copy[i][j] = Integer.MAX_VALUE;
+				visit[i][j] = Integer.MAX_VALUE; // 무한대로 초기화
 			}
 		}
-		result = Integer.MAX_VALUE;
-
-		bfs(0, 0);
 		
-		if(result == Integer.MAX_VALUE)
-			System.out.println(-1);
-		else
-			System.out.println(result);
+		ans = Integer.MAX_VALUE;
+		
+		bfs(0,0); // 출발점에서 bfs 시작
+		
+		if(ans == Integer.MAX_VALUE) System.out.println(-1);
+		else System.out.println(ans);;
 	}
-
+	
 	public static void bfs(int y, int x) {
-
-		Queue<H> q = new LinkedList<>();
-
-		q.add(new H(y, x, 1, 0));
-		copy[y][x] = 0; // 공사 횟수
-
-		while (!q.isEmpty()) {
-			H m = q.poll();
+		
+		Queue<Place> q = new LinkedList<>();
+		
+		q.add(new Place(y, x, 1, 0)); // y좌표, x좌표, 이동거리, 공사횟수
+		visit[y][x] = 0; // 처음 공사횟수
+		
+		while(!q.isEmpty()) {
 			
-			// 도착했을 때
-			if(m.y == N - 1 && m.x == M - 1) {
-				result = m.dist;
+			Place p = q.poll();
+			
+			// 도착지점 만나면 종료
+			if(p.y == N-1 && p.x == M-1){
+				ans = p.dis;
 				break;
 			}
 			
 			for (int i = 0; i < 4; i++) {
-				int ny = m.y + dy[i];
-				int nx = m.x + dx[i];
-
-				if (ny < 0 || ny >= N || nx < 0 || nx >= M)
-					continue;
-
-				if (copy[ny][nx] <= m.drill)
-					continue;
+				int ny = p.y + dy[i];
+				int nx = p.x + dx[i];
 				
-				if (map[ny][nx] == 0) {
-					copy[ny][nx] = m.drill;
-					q.add(new H(ny, nx, m.dist + 1, m.drill));
+				if(ny<0 || nx<0 || ny>=N || nx>=M) continue;
+				
+				if(visit[ny][nx] <= p.drill) continue;
+				
+				if(map[ny][nx] == 0) { // 벽이 아닐 때
+					visit[ny][nx] = p.drill;
+					q.add(new Place(ny, nx, p.dis+1, p.drill));
 				}
-				// 벽인 경우
-				else {
-					if (m.drill == 0) {
-						copy[ny][nx] = m.drill + 1;
-						q.add(new H(ny, nx, m.dist + 1, m.drill + 1));
+				else { // 벽일 때
+					if(p.drill == 0){
+						visit[ny][nx] = p.drill + 1;
+						q.add(new Place(ny, nx, p.dis+1, p.drill+1));
 					}
 				}
 			}
+			
 		}
+		
 	}
+
 }
