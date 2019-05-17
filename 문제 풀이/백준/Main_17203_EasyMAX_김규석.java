144a218c8104b3ec458cc1ee191ca44d2ae7f133
Gyuseok Kim <34904741+kim6394@users.noreply.github.com>
2019-05-17 21:22:29 +0900

    EasyMAX_규석(백준17203)

--- 문제 풀이/백준/Main_17203_EasyMAX_김규석.java
+++ 문제 풀이/백준/Main_17203_EasyMAX_김규석.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_17203_EasyMAX_±è±Ô¼® {
+public class Main_17203_EasyMAX_ {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
