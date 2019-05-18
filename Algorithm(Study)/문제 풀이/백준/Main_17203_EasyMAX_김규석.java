c96f8cde50a732352587d0379a10363e860bc719
Gyuseok Kim <34904741+kim6394@users.noreply.github.com>
2019-05-18 20:09:54 +0900

    EasyMAX_규석(백준17203)

---
+++ Algorithm(Study)/문제 풀이/백준/Main_17203_EasyMAX_김규석.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_17203_EasyMAX_김규석 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		int[] num = new int[N];
+		
+		for (int i = 0; i < num.length; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			int first = Integer.parseInt(st.nextToken()) - 1;
+			int second = Integer.parseInt(st.nextToken()) - 1;
+			
+			int sum = 0;
+			
+			for (int j = first; j < second; j++) {
+				
+				sum += Math.abs(num[j+1] - num[j]);
+				
+			}
+			
+			System.out.println(sum);
+		}
+	}
+
+}
