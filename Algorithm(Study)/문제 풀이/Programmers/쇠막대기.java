705658180eeaadd5c8f5a7a0ea917167945bc271
Gyuseok Kim <34904741+kim6394@users.noreply.github.com>
2019-09-20 08:41:05 +0900

    Create 쇠막대기.java

---
+++ Algorithm(Study)/문제 풀이/Programmers/쇠막대기.java
@@ -0,0 +1,29 @@
+public class 쇠막대기 {
+	
+	static char[] c;
+	static int cnt;
+	public int solution(String arrangement) {
+        int answer = 0;
+        
+        c = arrangement.toCharArray();
+        cnt = 0;
+        
+        for (int i = 0; i < c.length; i++) {
+        	
+        	if(c[i] == '(') {
+        		cnt++;
+        	}
+        	else { // ')'
+        		
+        		if(c[i-1] == ')') {
+        			answer += 1;
+        			cnt--;
+        		}
+        		else
+        			answer += (--cnt);
+        	}
+		}
+        
+        return answer;
+    }
+}
